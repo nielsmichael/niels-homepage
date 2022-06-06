faee9e39c0beaa8ade94b86c8478e771e5701ad0
xXnielsXx <nielsmichael@comcast.net>
2022-06-07 08:31:24 +0900

    Add items to navbar

--- components/navbar.js
+++ components/navbar.js
@@ -35,22 +35,45 @@ import {
 
    return (
      <Box
-      position="fixed"
-      as="nav"
-      w="100%"
-      bg={useColorModeValue('#E7ECEF', '#333333')}
-      style={{backdropFilter: 'blur11px'}}
-      zIndex={1}
-      {...props}
-      >
-        <Container display='flex' p={2} maxW="container.md" wrap="wrap" align="center" justify="space-between">
-          <Flex align="center" mr={5}>
-            <Heading as="h1" size="lg">
-              <Logo />
-            </Heading>
-          </Flex>
-        </Container>
-      </Box>
+       position="fixed"
+       as="nav"
+       w="100%"
+       bg={useColorModeValue('#E7ECEF', '#333333')}
+       style={{ backdropFilter: 'blur11px' }}
+       zIndex={1}
+       {...props}
+     >
+       <Container
+         display="flex"
+         p={2}
+         maxW="container.md"
+         wrap="wrap"
+         align="center"
+         justify="space-between"
+       >
+         <Flex align="center" mr={5}>
+           <Heading as="h1" size="lg">
+             <Logo />
+           </Heading>
+         </Flex>
+
+         <Stack
+           direction={{ base: 'column', md: 'row' }}
+           display={{ base: 'none', md: 'flex' }}
+           width={{ base: 'full', md: 'auto' }}
+           alignItems="center"
+           flexGrow={1}
+           mt={{ base: 4, nmd: 0 }}
+         >
+           <LinkItem href="/portfolio" path={path}>
+             Past Work
+           </LinkItem>
+           <LinkItem href="/contact" path={path}>
+             Contact
+           </LinkItem>
+         </Stack>
+       </Container>
+     </Box>
    )
  }
 
