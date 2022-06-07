5b6882cf8d51dbe2bd97b4c891d95535a00850c8
xXnielsXx <nielsmichael@comcast.net>
2022-06-07 09:04:57 +0900

    add mobile menu, create theme.js

--- components/navbar.js
+++ components/navbar.js
@@ -11,70 +11,96 @@ import {
   MenuList,
   MenuButton,
   IconButton,
-  useColorModeValue
- } from '@chakra-ui/react'
- import { HamburgerIcon } from '@chakra-ui/icons'
+  useColorModeValue,
+  Menu
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
 
- const LinkItem = ({ href, path, children }) => {
-   const active = path === href
-   const inactiveColor = useColorModeValue('#274C77', '#E7ECEF')
-   return (
-     <NextLink href={href}>
-       <Link
+const LinkItem = ({ href, path, children }) => {
+  const active = path === href
+  const inactiveColor = useColorModeValue('#274C77', '#E7ECEF')
+  return (
+    <NextLink href={href}>
+      <Link
+       p={2}
+       bg={active ? '#A3CEF1' : undefined}
+       color={active ? '#274C77' : inactiveColor}>
+         {children}
+       </Link>
+    </NextLink>
+  )
+}
+
+const Navbar = props => {
+  const { path } = props
+
+  return (
+    <Box
+      position="fixed"
+      as="nav"
+      w="100%"
+      bg={useColorModeValue('#E7ECEF', '#333333')}
+      style={{ backdropFilter: 'blur11px' }}
+      zIndex={1}
+      {...props}
+    >
+      <Container
+        display="flex"
         p={2}
-        bg={active ? '#A3CEF1' : undefined}
-        color={active ? '#274C77' : inactiveColor}>
-          {children}
-        </Link>
-     </NextLink>
-   )
- }
-
- const Navbar = props => {
-   const { path } = props
-
-   return (
-     <Box
-       position="fixed"
-       as="nav"
-       w="100%"
-       bg={useColorModeValue('#E7ECEF', '#333333')}
-       style={{ backdropFilter: 'blur11px' }}
-       zIndex={1}
-       {...props}
-     >
-       <Container
-         display="flex"
-         p={2}
-         maxW="container.md"
-         wrap="wrap"
-         align="center"
-         justify="space-between"
-       >
-         <Flex align="center" mr={5}>
-           <Heading as="h1" size="lg">
-             <Logo />
-           </Heading>
-         </Flex>
-
-         <Stack
-           direction={{ base: 'column', md: 'row' }}
-           display={{ base: 'none', md: 'flex' }}
-           width={{ base: 'full', md: 'auto' }}
-           alignItems="center"
-           flexGrow={1}
-           mt={{ base: 4, nmd: 0 }}
-         >
-           <LinkItem href="/portfolio" path={path}>
-             Past Work
-           </LinkItem>
-           <LinkItem href="/contact" path={path}>
-             Contact
-           </LinkItem>
-         </Stack>
-       </Container>
-     </Box>
-   )
- }
+        maxW="container.md"
+        wrap="wrap"
+        align="center"
+        justify="space-between"
+      >
+        <Flex align="center" mr={5}>
+          <Heading as="h1" size="lg">
+            <Logo />
+          </Heading>
+        </Flex>
+        <Stack
+          direction={{ base: 'column', md: 'row' }}
+          display={{ base: 'none', md: 'flex' }}
+          width={{ base: 'full', md: 'auto' }}
+          alignItems="center"
+          flexGrow={1}
+          mt={{ base: 4, nmd: 0 }}
+        >
+          <LinkItem href="/work" path={path}>
+            Work
+          </LinkItem>
+          <LinkItem href="/about" path={path}>
+            About
+          </LinkItem>
+        </Stack>
+        <Box flex={1} align="right">
+          <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                icon={<HamburgerIcon />}
+                variant="outline"
+                aria-label="Options"
+              />
+              <MenuList>
+                <NextLink href="/" passHref>
+                  <MenuItem as={Link}>Home</MenuItem>
+                </NextLink>
+                <NextLink href="/work" passHref>
+                  <MenuItem as={Link}>Work</MenuItem>
+                </NextLink>
+                <NextLink href="/about" passHref>
+                  <MenuItem as={Link}>About</MenuItem>
+                </NextLink>
+                <MenuItem as={Link} href="https://github.com/nielsmichael">
+                  Github
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  )
+}
 
 export default Navbar
