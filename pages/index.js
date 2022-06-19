9d35eb114a83d75d2f4f3276314daa26dc55c89f
xXnielsXx <nielsmichael@comcast.net>
2022-06-19 18:55:50 +0900

    Fix a few more colors for theme changes

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,24 @@
-import { Container, Box, Heading } from "@chakra-ui/react"
+import { Container, Box, Heading, useColorModeValue } from "@chakra-ui/react"
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="#A3CEF1" p={3} mb={6} align="center">
-        Hello! I&apos;m a web developer in Tokyo, Japan.
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('#A3CEF1', '#009EF6')}
+        p={3}
+        mb={6}
+        align="center"
+      >
+        Hello! I&apos;m a web developer living in Tokyo, Japan.
       </Box>
-      
-      <Box display={{md:'flex'}}>
+
+      <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
             Niels Michael
           </Heading>
-          <p>Web Developer / Designer</p>
+          <p>Front End & Web Developer / Designer</p>
         </Box>
       </Box>
     </Container>
