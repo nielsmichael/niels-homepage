e4a558d8371ec29b6c97850f0bd1244eca7a1d25
xXnielsXx <nielsmichael@comcast.net>
2022-06-28 09:33:11 +0900

    Add Work page

---
+++ pages/work.js
@@ -0,0 +1,13 @@
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+
+import Section from '../components/section'
+
+const Work = () => {
+  return (
+    <Container>
+      <Heading as='h2' fontSize={20} mb={4}>Work</Heading>
+    </Container>
+  )
+}
+
+export default Work
