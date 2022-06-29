26d0ed831512dc40fba21c0905ecb6a2fe3388d1
xXnielsXx <nielsmichael@comcast.net>
2022-06-29 18:14:54 +0900

    add about page

--- pages/blog.js
+++ pages/blog.js
@@ -1,17 +1,24 @@
-import { Container, Heading, SimpleGrid, } from '@chakra-ui/react'
+import { 
+  Container, 
+  Heading, 
+  // SimpleGrid, 
+} from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import { GridItem } from '../components/grid-link'
+// import { GridItem } from '../components/grid-link'
+import Blurb from '../components/blurb'
 
 const Blog = () => (
-  <Layout title="Posts">
+  <Layout title="Blog">
     <Container>
       <Heading as='h2' fontSize={20} mb={4}>Blog Posts</Heading>
       <Section delay={0.1}>
-        <GridItem>
+        <Blurb>
           No blog posts yet - but coming soon!
-        </GridItem>
+        </Blurb>
       </Section>
     </Container>
   </Layout>
 )
+
+export default Blog
