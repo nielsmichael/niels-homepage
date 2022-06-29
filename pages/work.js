b6d1590a6f379d91ec3e2ad5ebda62d61abd61bf
xXnielsXx <nielsmichael@comcast.net>
2022-06-29 12:15:08 +0900

    Finish work items (for now)

--- pages/work.js
+++ pages/work.js
@@ -2,7 +2,9 @@ import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-link'
-import makotoThumb from '../public/img/work/makoto-investments.png'
+import makotoThumb from '../public/img/work/makoto-investments.jpg'
+import anchorThumb from '../public/img/work/anchor-investments.jpg'
+import nielsMealsThumb from '../public/img/work/niels-meals.jpg'
 
 const Work = () => {
   return (
@@ -12,16 +14,35 @@ const Work = () => {
       </Heading>
 
       <SimpleGrid columns={[1, 1, 2]} gap={6}>
-        <Section>
+        <Section delay={0.1}>
           <WorkGridItem
-            id="makotoInvestments"
+            id="makoto-investments"
             title="Makoto Investments"
             thumbnail={makotoThumb}
           >
-            Website homepage (English ver.) for Makoto Investments Ltd.
+            Website homepage (English ver.) for consulting firm Makoto Investments Ltd. Built with HTML, CSS, and JavaScript (JQuery).
           </WorkGridItem>
         </Section>
-        
+        <Section delay={0.2}>
+          <WorkGridItem
+            id="anchor-investments"
+            title="Anchor Investments"
+            thumbnail={anchorThumb}
+          >
+            Website homepage for real estate acquisition and property management company Anchor Investments. Built with NextJS and Typescript.
+          </WorkGridItem>
+        </Section>
+        <Section delay={0.3}>
+          <WorkGridItem
+            id="niels-meals"
+            title="Niels' Meals"
+            thumbnail={nielsMealsThumb}
+          >
+            An online ordering front-end interface built with React.
+            Unfortunately, I have not opened my restaurant yet - but when I do,
+            the web app is ready to go.
+          </WorkGridItem>
+        </Section>
       </SimpleGrid>
     </Container>
   )
