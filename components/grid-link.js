ea1c112f6d3b7cc58aa0577ece6b92a35c642635
xXnielsXx <nielsmichael@comcast.net>
2022-06-29 11:27:55 +0900

    Add Makoto site to work page

--- components/grid-link.js
+++ components/grid-link.js
@@ -1,6 +1,6 @@
 import NextLink from 'next/link'
 import Image from 'next/image'
-import { Box, Text, LinkBox, LinkOverlay, LinkBox } from '@chakra-ui/react'
+import { Box, Text, LinkBox, LinkOverlay } from '@chakra-ui/react'
 import { Global } from '@emotion/react'
 
 export const GridItem = ({ children, href, title, thumbnail }) => (
@@ -20,7 +20,23 @@ export const WorkGridItem = ({ children, id, title, thumbnail }) => (
     <NextLink href={`/work/${id}`}>
       <LinkBox cursor='pointer'>
         <Image src={thumbnail} alt={title} className='grid-item-thumbnail' placeholder='blur' />
+        <LinkOverlay href={`/work/${id}`} >
+          <Text mt={2} fontSize={20}>
+            {title}
+          </Text>
+        </LinkOverlay>
+        <Text fontSize={14}>{children}</Text>
       </LinkBox>
     </NextLink>
   </Box>
+)
+
+export const GridItemStyle = () => (
+  <Global
+    styles={`
+      .grid-item-thumbnail {
+        border-radius: 12px;
+      }
+    `}
+  />
 )
--- pages/work.js
+++ pages/work.js
@@ -1,11 +1,28 @@
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 
 import Section from '../components/section'
+import { WorkGridItem } from '../components/grid-link'
+import makotoThumb from '../public/img/work/makoto-investments.png'
 
 const Work = () => {
   return (
     <Container>
-      <Heading as='h2' fontSize={20} mb={4}>Work</Heading>
+      <Heading as="h2" fontSize={20} mb={4}>
+        Work
+      </Heading>
+
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        <Section>
+          <WorkGridItem
+            id="makotoInvestments"
+            title="Makoto Investments"
+            thumbnail={makotoThumb}
+          >
+            Website homepage (English ver.) for Makoto Investments Ltd.
+          </WorkGridItem>
+        </Section>
+        
+      </SimpleGrid>
     </Container>
   )
 }
