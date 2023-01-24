fc363caaf95cbf19e4d14dfadf334fc198ac4b39
xXnielsXx <nielsmichael@comcast.net>
2023-01-24 14:56:05 +0900

    Resolve path in next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
 
   i18n: {
     localeDetection: true,
     locales: ['en', 'ja'],
-    defaultLocale: 'en'
+    defaultLocale: 'en',
+    localePath: path.resolve('./locales')
   }
 }
